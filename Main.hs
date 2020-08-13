a1a4d05dd2ae43369dcc4828817fd7a4d93dc71a
Alexandre Peyroux <alex@px.io>
2020-08-13 23:47:55 +0200

    snap

--- Main.hs
+++ Main.hs
@@ -13,11 +13,11 @@ import           Test.WebDriver
 import           Test.WebDriver.Exceptions
 
 firefoxConfig :: WDConfig
-firefoxConfig = useBrowser firefox defaultConfig 
+firefoxConfig = useBrowser firefox { ffAcceptInsecureCerts = (Just True)  }  defaultConfig 
 
 chromeConfig :: WDConfig
 -- chromeConfig = useBrowser (chrome { chromeBinary = Just "/nix/store/rsssqjzxs9c25ir9mzq074k50l26fzgp-user-environment/bin/google-chrome-stable", chromeOptions = ["--no-startup-window"] }) defaultConfig
--- chromeConfig = useBrowser (chrome { chromeBinary = Just "/nix/store/rsssqjzxs9c25ir9mzq074k50l26fzgp-user-environment/bin/google-chrome-stable" }) defaultConfig
+-- chromeConfig = useBrowser (chrome { chromeBinary = Just "/nix/store/q31sq1y9kwbcdx2d97iz48zpqrdhnl1a-chromium-83.0.4103.97/bin/chromium" }) defaultConfig
 chromeConfig = useBrowser chrome defaultConfig
 
 phantomjsConfig :: WDConfig
@@ -28,27 +28,29 @@ checkInstance instanceName = do
 
   -- putStrLn $ "Start du test pour " <> (T.unpack instanceURL) <> " ..."
 
-  runSession chromeConfig . finallyClose $ do
+  runSession (firefoxConfig { wdHTTPRetryCount = 2 }) . finallyClose $ do
 
     openPage (T.unpack instanceURL)
   
     loginInput <- findElem (ByCSS "input[type='text']")
     passwordInput <- findElem (ByCSS "input[type='password']")
+    loginButton <- findElem (ByCSS "input[class='login_button']")
   
-    sendKeys "Administrator" loginInput
-    sendKeys "Administrator" passwordInput
+    sendKeys "lrpgn" loginInput
+    sendKeys "nvw6xvkoyo" passwordInput
   
-    submit passwordInput
+    -- submit loginButton 
+    click loginButton 
 
-    t <- getTitle
-    liftIO $ putStrLn $ "Ouverture de la page: " <> T.unpack t
+    -- t <- getTitle
+    -- liftIO $ putStrLn $ "Ouverture de la page: " <> T.unpack t
 
-    divServiceBlock <- try $ findElem (ByCSS "div[class='userServicesBlock']")
+    divServiceBlock <- try $ findElem (ByCSS "div[class='menu']")
     case (divServiceBlock :: Either FailedCommand Element) of
       Left (FailedCommand t _) -> do
-        liftIO $ putStrLn $ (T.unpack instanceName) <> " [KO]"
-        saveScreenshot $ "/tmp/snap/error-" <> (T.unpack instanceName) <> ".jpg"
-        liftIO $ putStrLn $ show t
+        liftIO $ putStrLn $ (T.unpack instanceName) <> " [KO] " <> show t 
+        saveScreenshot $ "/tmp/snap/error-" <> (T.unpack instanceName) <> ".png"
+        -- liftIO $ putStrLn $ show t
       Right _ -> do
         -- t <- getText sb
         -- liftIO $ print t
@@ -57,7 +59,7 @@ checkInstance instanceName = do
   -- putStrLn "Fin du test ..."
 
   where
-    instanceURL = "https://" <> instanceName <> ".krb.gendarmerie.fr"
+    instanceURL = "https://atrc-" <> instanceName <> ".krb.gendarmerie.fr/nuxeo/login.jsp"
 
 
 main :: IO ()
@@ -68,7 +70,8 @@ main = do
                         r <- try (checkInstance i)
                         case (r :: Either FailedCommand ()) of
                           Left (FailedCommand t _) -> do
-                            putStrLn $ (T.unpack i) <> " [KO]" -- "ERROR: " <> show t
+                            -- putStrLn $ (T.unpack i) <> " [KO]" -- "ERROR: " <> show t
+                            return ()
                           Right _ -> do
                             return ()
                             -- putStrLn "Traitement OK !!!!"
